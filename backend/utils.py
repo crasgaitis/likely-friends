eb8930983135128a440c09cd3952f1ebcf5ea2e4
crasgaitis <crasgaitis@gmail.com>
2024-07-07 15:27:16 -0700

    base

--- backend/utils.py
+++ backend/utils.py
@@ -8,6 +8,6 @@ def generate_quiz_code(length=6):
     characters = string.ascii_uppercase + string.digits
     while True:
         code = ''.join(secrets.choice(characters) for _ in range(length))
-        # Check if the generated code is unique
+        # check if code is unique
         if not Quiz.query.filter_by(code=code).first():
             return code
